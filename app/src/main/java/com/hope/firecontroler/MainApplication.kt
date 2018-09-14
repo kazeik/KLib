c73951f5502d6402faa4e73ffbf15adaf1ee5d97
kazeik@163.com <chenkai851206>
2018-09-14 14:08:13 +0800

    添加Application

---
+++ app/src/main/java/com/hope/firecontroler/MainApplication.kt
@@ -0,0 +1,14 @@
+package com.hope.firecontroler
+
+import android.app.Application
+
+/**
+ * @author kazeik.chen, QQ:77132995, email:kazeik@163.com
+ * 2018 09 14 14:07
+ * 类说明:
+ */
+class MainApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
